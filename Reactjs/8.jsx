e9b5050d0d9037b6e28356efc30e77c40e536a68
chwla <chwla75@gmail.com>
2025-06-20 19:16:36 +0530

    react 8 usestate hook

---
+++ Reactjs/8.jsx
@@ -0,0 +1,20 @@
+//Hooks
+
+//Life Cycle of a component
+    //Constructed then Mounted (rendered on screen) then Updated (re-rendered) then Un-Mounted then back to Constructed  
+
+//useState Hook- A state of a component is a variable that holds some information that may change over the lifetime of a component. 
+            //Whenever the value of the state changes, the component re-renders itself with updated value.
+
+//useState Hook example-
+
+import React, {useState} from 'react';
+function CounterComponent(){
+        const [count, setCount] = useState(0);
+    return (
+        <div>
+            <p>Count is {count}</p>
+            <button onClick={() => setCount(count+1)}>Increment</button>
+        </div>
+    )
+}
