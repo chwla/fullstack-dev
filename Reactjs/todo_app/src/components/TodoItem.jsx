67c7af2ef6f50c896f9d17ebba6607d397c7615e
chwla <chwla75@gmail.com>
2025-06-20 18:26:08 +0530

    react 6 todo

---
+++ Reactjs/todo_app/src/components/TodoItem.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const TodoItem = ()=>{
+    return (
+        <li className="todo-item">
+            <span>
+            <input type="checkbox" />
+            <span className="todo-item-text">DSA</span>
+            </span>
+            <p>...</p>
+        </li>
+    )
+}
+
+export default TodoItem;
