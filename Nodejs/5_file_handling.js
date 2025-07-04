4665f43e07e064260fe71978ed125b690817d568
chwla <chwla75@gmail.com>
2025-07-04 16:42:54 +0530

    nodejs file handling

---
+++ Nodejs/5_file_handling.js
@@ -0,0 +1,31 @@
+//File Handling
+
+const fs = rqeuire("fs");
+
+//Sync
+fs.writeFileSync("./test.txt", "Hello World");
+
+//Async
+fs.writeFile("./test.txt", "Hello World", (err)=>{});
+
+fs.readFileSync("./test.txt", "utf-8");
+
+fs.readFile("./tets.txt", "utf-8", (err, result)=>{
+    if(err){
+        console.log("Error", err);
+    }
+    else{
+        console.log(result);
+    }
+});
+
+
+fs.appendFileSync("./test.txt", `${Date.now()}Hey THere\n`);
+
+fs.cpSync("./test.txt", "./copy.txt");
+
+fs.unlinkSync("./copy.txt");
+
+fs.statSync("./text.txt");
+
+fs.mkdirSync("my-docss/a/b", {recursive: true});
