933314eb1eac23e64a89c59dc81ca1d31a12fbb1
chwla <chwla75@gmail.com>
2025-07-04 17:23:13 +0530

    nodejs handling url

--- Nodejs/6_http_server/index.js
+++ Nodejs/6_http_server/index.js
@@ -1,20 +1,30 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
-const myServer = http.createServer((req, res)=>{
+const myServer = http.createServer((req, res) => {
+    const myUrl = url.parse(req.url, true); // parse query as object
     const log = `${Date.now()}: ${req.url} New Req Received\n`;
-    fs.appendFile('log.txt', log, (err,data)=>{
-        switch(req.url){
-            case '/': 
+
+    fs.appendFile('log.txt', log, (err) => {
+        if (err) {
+            console.error("Log write error:", err);
+        }
+
+        switch (myUrl.pathname) {
+            case '/':
                 res.end("Home");
                 break;
-            case '/about': 
-                res.end("about");
+
+            case '/about':
+                const username = myUrl.query.myname || "Guest"; // fallback if myname not passed
+                res.end(`Hi, ${username}`);
                 break;
+
             default:
-                res.end("NOt found");
+                res.end("Not found");
         }
     });
 });
 
-myServer.listen(8000, ()=>console.log("Server Started"));
+myServer.listen(8000, () => console.log("Server Started"));
