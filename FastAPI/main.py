83642961b69485193ebd32dfd43ee90bd9e3cf50
chwla <chwla75@gmail.com>
2025-07-12 19:56:26 +0530

    fastapi completed section 7

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, List
 from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
 from pydantic import BaseModel 
@@ -7,7 +7,7 @@ from psycopg2.extras import RealDictCursor
 import time
 from dotenv import load_dotenv
 import os
-from . import models
+from . import models, schemas
 from .database import engine, get_db
 from sqlalchemy.orm import Session
 from sqlalchemy import String
@@ -19,11 +19,6 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# Utility function to find a post by id
-def find_posts(id):
-    for p in my_posts:
-        if p["id"] == id:
-            return p
 
 # Root route
 @app.get("/")
@@ -35,14 +30,14 @@ def root():
 def test_posts(db: Session = Depends(get_db)):
     
     posts = db.query(models.Post).all()
-    return{"data": posts}
+    return posts
 
 # Return all posts
-@app.get("/posts")
-def get_posts():
-    cursor.execute("""SELECT * FROM posts """)
-    posts = cursor.fetchall()
-    return {"data": posts}
+@app.get("/posts", response_model = List[schemas.Post])
+def get_posts(db: Session = Depends(get_db)):
+    
+    posts = db.query(models.Post).all()
+    return posts
 
 ######################################################
 
@@ -53,30 +48,26 @@ def get_posts():
 #     return {"new_post": f"title {payLoad['title']} content: {payLoad['content']}"}
 
 # With Pydantic (will do validation automatically)
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = True
 
 # Create a new post with validation
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_posts(post: Post, db: Session = Depends(get_db)):
+@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model = schemas.Post)
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
     new_post = models.Post(**post.dict())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
-    return {"data": post}
+    return post
        
 
 # Get a single post by ID
-@app.get("/posts/{id}")
+@app.get("/posts/{id}", response_model = schemas.Post)
 def get_post(id: int, db: Session = Depends(get_db)):
     post = db.query(models.Post).filter(models.Post.id == id).first()
     print(post)
 
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
-    return{"post_detail": post}
+    return post
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db)):
@@ -91,8 +82,8 @@ def delete_post(id: int, db: Session = Depends(get_db)):
 
     return Response(status_code=status.HTTP_204_NO_CONTENT)
  
-@app.put("/posts/{id}")
-def update_post(id: int, updated_post: Post, db: Session = Depends(get_db)):
+@app.put("/posts/{id}", response_model = schemas.Post)
+def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
     
     post_query = db.query(models.Post).filter(models.Post.id == id)
     post = post_query.first()
@@ -102,7 +93,7 @@ def update_post(id: int, updated_post: Post, db: Session = Depends(get_db)):
 
     post_query.update(updated_post.dict(), synchronize_session = False)
     db.commit()
-    return {"data", post_query.first()}
+    return post_query.first()
 
 
 while True:
